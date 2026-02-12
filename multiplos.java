4350189a64fe0e50737a55d0aa6c58f5ae55dc4b
Bychucee <Saulmejiapolo123@gmail.com>
2026-02-12 12:22:38 +0100

    Version_Inicial

--- multiplos.java
+++ multiplos.java
@@ -2,10 +2,10 @@ public static void multiplos(int numeroMax) {
     
     for (int i=0;i<=numeroMax;i++) {
         if(i%49==0) {
-            System.out.println("Multiplo de 49: "+i);
+            System.out.println("Múltiplo de 49: "+i);
         }
         else if(i%100==0) {
-            System.out.println("Multiplo de 100: "+i);
+            System.out.println("Múltiplo de 100: "+i);
         }
     }
 }
