eb57b40470cbe2a65b31a5230468c9ea02ee0ee2
Bychucee <Saulmejiapolo123@gmail.com>
2026-02-12 12:33:03 +0100

    VerisonFinal

--- multiplos.java
+++ multiplos.java
@@ -2,13 +2,13 @@ public static void multiplos(int numeroMax) {
     
     for (int i=0;i<=numeroMax;i++) {
         if(i%7==0) {
-            System.out.println("Múltiplo de 7: "+i);
+            System.out.println("Múltiplos de 7: "+i);
         }
         else if(i%49==0) {
-            System.out.println("Múltiplo de 49: "+i);
+            System.out.println("Múltiplos de 49: "+i);
         }
         else if(i%100==0) {
-            System.out.println("Múltiplo de 100: "+i);
+            System.out.println("Múltiplos de 100: "+i);
         }
     }
 }
